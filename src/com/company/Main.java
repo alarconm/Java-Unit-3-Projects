343779685fa563f1aebff57a03f136bdc9b01e05
Michael Alarcon <malarconii@yahoo.com>
2017-07-27 22:40:53 -0700

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Java");
+        int number = 1+1;
+        System.out.println(number);
+    }
+}
