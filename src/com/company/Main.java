03e230ba7d1fcbec09f71c6bdcb50f04b38093b4
Michael Alarcon <malarconii@yahoo.com>
2017-08-06 17:16:33 -0700

    finished arraylist

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Java");
-        int number = 1+1;
+    int number = 1+1;
         System.out.println(number);
-    }
+}
 }
